6851d5427f2097207e4868614431054040055065
alanbebermartins <abebermartins@gmail.com>
2024-06-14 08:48:06 -0300

    Atualização de código

--- backend/app/crud/task_crud.py
+++ backend/app/crud/task_crud.py
@@ -3,6 +3,14 @@ from app.models.task_models import Task
 from app.database import SessionLocal
 
 def create_task(task_title: str, task_description: str):
+    # Verificar o comprimento do título da tarefa
+    if len(task_title) > 20:
+        return "O título da tarefa deve possuir no máximo 20 caracteres"
+    
+    # Verificar o comprimento da descrição da tarefa
+    if len(task_description) > 100:
+        return "A descrição da tarefa deve possuir no máximo 100 caracteres"
+    
     db = SessionLocal()
     try:
         # Criar uma nova instância de Task
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,23 @@
 
 from app.crud.task_crud import create_task
 
-# Criar uma nova tarefa
-new_task = create_task(task_title="Montar Cronograma", task_description="Montar o planejamento das tasks no jira, baseado no tamanho da equipe e tecnologias utlizadas")
+# Tentar criar uma nova tarefa com título e descrição válidos
+result = create_task(task_title="loja de ferramentas", task_description="Comprar ferramentas e material para conserto na lavanderia")
+if isinstance(result, str):
+    print(f"Erro: {result}")
+else:
+    print(f"Tarefa criada com sucesso! ID: {result.task_id}, Título: {result.task_title}, Descrição: {result.task_description}")
 
-print(f"Tarefa criada com sucesso! ID: {new_task.task_id}, Título: {new_task.task_title}, Descrição: {new_task.task_description}")
+# Tentar criar uma nova tarefa com um título muito longo
+result = create_task(task_title="Título muito longo que excede 20 caracteres", task_description="Comprar leite integral no supermercado")
+if isinstance(result, str):
+    print(f"Erro: {result}")
+else:
+    print(f"Tarefa criada com sucesso! ID: {result.task_id}, Título: {result.task_title}, Descrição: {result.task_description}")
+
+# Tentar criar uma nova tarefa com uma descrição muito longa
+result = create_task(task_title="Comprar leite", task_description="Descrição muito longa que excede 100 caracteres. Descrição muito longa que excede 100 caracteres. Descrição muito longa que excede 100 caracteres.")
+if isinstance(result, str):
+    print(f"Erro: {result}")
+else:
+    print(f"Tarefa criada com sucesso! ID: {result.task_id}, Título: {result.task_title}, Descrição: {result.task_description}")
